16e323f2cc9964e93f95ba57a2621b9b30f733ab
Kamila <kmilaporto1991@gmail.com>
2021-03-09 09:32:26 -0300

    modulo 1- sobre a jpa

---
+++ README.md
@@ -0,0 +1,18 @@
+# OrangeTalents-JPA-e-Hibernate
+
+### Modulo 1 - Sobre a Java Persistence API(JPA)
+
+Como é feito a persistência de dados sem usar JPA?
+* especificação JDBC - possibilita o reuso de código para nos conectarmos com qualquer banco de dados geracional
+
+O que é JPA? Hibernate?
+* JPA é um ORM (Object Relacional Mapper) Java;
+	* um ORM mapeia as classes para tabelas e gera o SQL de forma automática;
+* JPA é uma especificação no mundo Java que cuida do papel de persistência com mapeamento-objeto-relacional. 
+* Hibernate é a a implementação de referência do JPA;
+* para inicializar a JPA, é preciso definir um arquivo de configuração, chamado de persistence.xml
+	* nesse arquivo está todas as configurações que precisamos dentro de uma unidade de persistência para acessarmos o banco;
+* podemos usar JPA para gerar e atualizar as tabelas no banco de dados;
+* EntityManager - Interface JPA, por meio dela podemos abstrair o mundo relacional e focar apenas em objetos;
+* através da classe Persistence é possível lê as configurações do arquivo persistence.xml e então criar uma EntitiManagerFactory;
+* toda entidade deve possuir a annotation @Entity e possuir um identificador anotado como @Id;
---
+++ src/main/java/br/com/alura/modelo/Conta.java
@@ -0,0 +1,66 @@
+package br.com.alura.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titular;
+    private Long agencia;
+    private Long numero;
+    private Double saldo;
+
+    public Conta(Long agencia, Long numero, String titular) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.titular = titular;
+    }
+
+    public Conta() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public Long getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Long agencia) {
+        this.agencia = agencia;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
